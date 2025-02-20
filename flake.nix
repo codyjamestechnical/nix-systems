c0ae7da8516382a96fa158adb0a1a01bace6029d
Cody James <cody@cjtech.io>
2025-02-19 22:12:47 -0500

    test

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     sops-nix.url = "github:Mic92/sops-nix";
   };
 
