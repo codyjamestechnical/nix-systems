11a045b5d7cc059e8cb62253d2b268209c73ba21
Cody James <cody@cjtech.io>
2025-02-21 17:23:21 -0500

    test

--- hosts/mars-server/default.nix
+++ hosts/mars-server/default.nix
@@ -133,7 +133,19 @@ services.avahi = {
     # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
 	enable = true;
     openFirewall = true;
-    
+    extraServiceFiles = {
+      smb = ''
+        <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+        <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+        <service-group>
+          <name replace-wildcards="yes">%h</name>
+          <service>
+            <type>_smb._tcp</type>
+          </service>
+        </service-group>
+      '';
+    };
+
 };
 
 services.samba-wsdd = {
