1de8cd5517acf2615b5afc98f062c767513adafa
Cody James <cody@cjtech.io>
2025-02-20 22:56:49 -0500

    removed btrfs support

--- modules/core.nix
+++ modules/core.nix
@@ -5,7 +5,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.supportedFilesystems = [ "btrfs" ];
 
   # Enable networking
   networking.networkmanager.enable = true;
