04f9c0e64b13919c37ac9743d3b7098271c6a4ed
Cody James <cody@cjtech.io>
2025-02-21 17:19:10 -0500

    tset

--- hosts/mars-server/default.nix
+++ hosts/mars-server/default.nix
@@ -124,11 +124,16 @@ services.samba = {
 services.avahi = {
     publish.enable = true;
     publish.userServices = true;
+    publish.hinfo = true;
     # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
     nssmdns4 = true;
+    nssmdns6 = true;
+    ipv6 = true;
+    hostName = "mars-server";
     # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
 	enable = true;
     openFirewall = true;
+    
 };
 
 services.samba-wsdd = {
