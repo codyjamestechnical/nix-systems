46d3da24b86c9fb9a6017bd36ce908383dbfb460
Cody James <cody@cjtech.io>
2025-02-21 22:39:32 -0500

    moved docker container config to machine specific file

--- hosts/mars-server/default.nix
+++ hosts/mars-server/default.nix
@@ -145,8 +145,47 @@
             ${backendBin} network create --subnet=${network_subnet} --gateway=${network_gateway} --aux-address 'host=${network_starting_ip}' --ip-range ${network_ip_range} --driver=macvlan -o parent=${network_parent_interface} ${network_name}
             ";
     };
- 
 
+    # Docker Containers
+    virtualisation.oci-containers = {
+        backend = "docker";
+        containers = {
+            komodo-peripherie = {
+                image = "ghcr.io/mbecker20/periphery:latest";
+                ports = ["0.0.0.0:8120:8120"];
+                log-driver = "local";
+                volumes = [
+                    "/var/run/docker.sock:/var/run/docker.sock"
+                    "/proc:/proc"
+                    "/etc/komodo/ssl:/etc/komodo/ssl"
+                    "/etc/komodo/repos:/etc/komodo/repos"
+                    "/etc/komodo/stacks:/etc/komodo/stacks"
+                    "/var/secrets/komodo-passkey:/var/secrets/passkey"
+                ];
+                environment = {
+                    PERIPHERY_SSL_ENABLED = "true";
+                    PERIPHERY_INCLUDE_DISK_MOUNTS = "/etc/hostname";
+                    PERIPHERY_PASSKEYS_FILE = "/var/secrets/passkey";
+                };
+                labels = {"komodo.skip" = "";};
+            };
+
+            beszel-agent = {
+                image = "henrygd/beszel-agent";
+                serviceName = "beszel-agent";
+                extraOptions = ["--network=host"];
+                volumes = [
+                    "/var/run/docker.sock:/var/run/docker.sock:ro"
+                    "/docker-data/.beszel:/extra-filesystems/nvme0n1p1:ro"
+                ];
+                environment = {
+                    PORT = "45876";
+                    KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZ+L7U8f/hxIu5fj0fTVT2ngKHo4Kv+SaSdEbat25cA";
+                };
+            };
+        };
+        
+    };
 
 
 
