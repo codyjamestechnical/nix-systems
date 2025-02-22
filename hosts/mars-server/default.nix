8f6fe5361981451683f201d5a6887bb0ef157287
Cody James <cody@cjtech.io>
2025-02-22 10:49:30 -0500

    fixed avahi nssdns to use the new nssdns4/6 flag

--- hosts/mars-server/default.nix
+++ hosts/mars-server/default.nix
@@ -93,7 +93,8 @@
 
     services.avahi = {
         enable = true;
-        nssmdns = true;
+        nssmdns4 = true;
+        nssmdns6 = true;
         publish = {
             enable = true;
             addresses = true;
