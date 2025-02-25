3c01d8a637dd471789c4c535c9cd5cbda125b5a2
Cody James <33857557+codyjamestechnical@users.noreply.github.com>
2025-02-25 09:34:13 -0500

    Update hardware-configuration.nix for deimos server since redeployment

--- hosts/deimos-server/hardware-configuration.nix
+++ hosts/deimos-server/hardware-configuration.nix
@@ -6,9 +6,9 @@
     efiInstallAsRemovable = true;
     device = "nodev";
   };
-  fileSystems."/boot" = { device = "/dev/disk/by-uuid/66DD-D3EB"; fsType = "vfat"; };
+  fileSystems."/boot" = { device = "/dev/disk/by-uuid/B564-D046"; fsType = "vfat"; };
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
-  
+
 }
