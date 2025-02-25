3f040d70d50c73384f9d7895dea460a6c2d501d5
Cody James <33857557+codyjamestechnical@users.noreply.github.com>
2025-02-25 09:35:23 -0500

    Update networking.nix for deimos-server since the rebuild

--- hosts/deimos-server/networking.nix
+++ hosts/deimos-server/networking.nix
@@ -2,7 +2,9 @@
   # This file was populated at runtime with the networking
   # details gathered from the active system.
   networking = {
-    nameservers = [ "8.8.8.8"
+    nameservers = [ "2a01:4ff:ff00::add:1"
+ "2a01:4ff:ff00::add:2"
+ "185.12.64.2"
  ];
     defaultGateway = "172.31.1.1";
     defaultGateway6 = {
@@ -18,23 +20,16 @@
         ];
         ipv6.addresses = [
           { address="2a01:4f8:c013:9662::1"; prefixLength=64; }
-{ address="fe80::9400:4ff:fe17:c056"; prefixLength=64; }
+{ address="fe80::9400:4ff:fe18:36ab"; prefixLength=64; }
         ];
         ipv4.routes = [ { address = "172.31.1.1"; prefixLength = 32; } ];
         ipv6.routes = [ { address = "fe80::1"; prefixLength = 128; } ];
       };
-            enp7s0 = {
-        ipv4.addresses = [
-          { address="10.0.0.2"; prefixLength=32; }
-        ];
-        ipv6.addresses = [
-          { address="fe80::8400:ff:fed5:55ae"; prefixLength=64; }
-        ];
-        };
+
     };
   };
   services.udev.extraRules = ''
-    ATTR{address}=="96:00:04:17:c0:56", NAME="eth0"
-    ATTR{address}=="86:00:00:d5:55:ae", NAME="enp7s0"
+    ATTR{address}=="96:00:04:18:36:ab", NAME="eth0"
+
   '';
 }
