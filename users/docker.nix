c0a3b315dc3e24739081dd9d8ff3a159486e355a
Cody James <cody@cjtech.io>
2025-02-25 19:37:06 -0500

    added docker to the acme group

--- users/docker.nix
+++ users/docker.nix
@@ -4,16 +4,14 @@
         defaultUserShell = pkgs.zsh;
         groups = {
             acme.gid = 984;
-            ssl.gid = 2012;
         };     
 
         users.docker = {
             isNormalUser = false;
             isSystemUser = true;
-            uid = 2013;
             group = "docker";
             extraGroups = [
-                "ssl"
+                "acme"
             ];
         };
     };
