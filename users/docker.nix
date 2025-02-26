b9b9c585ed9cc174dfc201e01853e07612662d88
Cody James <cody@cjtech.io>
2025-02-25 19:27:04 -0500

    set docker user to a static uid

--- users/docker.nix
+++ users/docker.nix
@@ -5,11 +5,13 @@
         groups = {
             acme.gid = 984;
             ssl.gid = 2012;
+            docker.gid = 2013;
         };     
 
         users.docker = {
             isNormalUser = false;
             isSystemUser = true;
+            uid = 2013;
             group = "docker";
             extraGroups = [
                 "ssl"
