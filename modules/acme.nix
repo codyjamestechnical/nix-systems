3fa50b1c8929e953018d84e4adca1690726e8909
Cody James <cody@cjtech.io>
2025-03-06 12:31:25 -0500

    set acme renewal interval to monthly

--- modules/acme.nix
+++ modules/acme.nix
@@ -3,11 +3,14 @@
     # Now we can configure ACME
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "cody@31337.im";
+  security.acme.defaults = {
+    postRun = "docker restart $(docker ps -q)";
+    renewInterval = "monthly";
+  };
   security.acme.certs."31337.im" = {
     domain = "*.31337.im";
     dnsProvider = "cloudflare";
     environmentFile = "/var/secrets/cloudflare-token";
     dnsPropagationCheck = true;
-    postRun = "docker restart $(docker ps -q)";
   };
 }
