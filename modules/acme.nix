841716e855fa70408e4ae27d3c08222a81de3082
Cody James <cody@cjtech.io>
2025-05-15 19:43:15 -0400

    removed the restart command from acme

--- modules/acme.nix
+++ modules/acme.nix
@@ -5,7 +5,7 @@
   security.acme.defaults.email = "cody@31337.im";
   security.acme.defaults = {
     # Restart all containers that use the caddy image after renewal
-    postRun = "docker ps -a --filter 'ancestor=caddy' --format '{{.ID}}' | xargs docker restart";
+    # postRun = "docker ps -a --filter 'ancestor=caddy' --format '{{.ID}}' | xargs docker restart";
     renewInterval = "monthly";
   };
   security.acme.certs."31337.im" = {
