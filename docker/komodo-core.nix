80d8f0a61c3a4d18cb41d16ba6106faa9e11582b
Cody James <cody@cjtech.io>
2026-01-09 17:53:37 -0500

    Improved layout of the komodo core config file

--- docker/komodo-core.nix
+++ docker/komodo-core.nix
@@ -3,92 +3,87 @@
 
 {
   # Containers
-  virtualisation.oci-containers.containers."komodo-caddy" = {
-    image = "caddy:latest";
-    environmentFiles = [
-      "/docker-data/Komodo/.env"
-    ];
-    volumes = [
-      "/docker-data/Komodo/caddy:/data:rw"
-      "/docker-data/Komodo/caddy/config:/config:rw"
-      "/docker-data/Komodo/caddyfile:/etc/caddy/Caddyfile:ro"
-      "/var/lib/acme/31337.im/fullchain.pem:/ssl/fullchain.pem:ro"
-      "/var/lib/acme/31337.im/key.pem:/ssl/privkey.pem:ro"
-    ];
-    ports = [
-      "80:80/tcp"
-      "443:443/tcp"
-    ];
-    log-driver = "journald";
-    extraOptions = [
-      "--cap-add=NET_ADMIN"
-      "--network-alias=caddy"
-      "--network=komodo_komodo-internal"
-    ];
-  };
+  virtualisation.oci-containers.containers = {
 
-  virtualisation.oci-containers.containers."komodo-periphery" = {
-    image = "ghcr.io/moghtech/komodo-periphery:latest";
-    log-driver = "local";
-    volumes = [
-        "/var/run/docker.sock:/var/run/docker.sock"
-        
-        "/etc/komodo/ssl:/etc/komodo/ssl"
-        "/etc/komodo/repos:/etc/komodo/repos"
-        "/etc/komodo/stacks:/etc/komodo/stacks"
-        "/var/secrets/komodo-passkey:/var/secrets/passkey"
-    ];
-    environment = {
-        PERIPHERY_SSL_ENABLED = "true";
-        PERIPHERY_INCLUDE_DISK_MOUNTS = "/etc/hostname";
-        PERIPHERY_PASSKEYS_FILE = "/var/secrets/passkey";
+    "komodo-caddy" = {
+      image = "caddy:latest";
+      labels = {"komodo.skip" = "";};
+      environmentFiles = [
+        "/docker-data/Komodo/.env"
+      ];
+      volumes = [
+        "/docker-data/Komodo/caddy:/data:rw"
+        "/docker-data/Komodo/caddy/config:/config:rw"
+        "/docker-data/Komodo/caddyfile:/etc/caddy/Caddyfile:ro"
+        "/var/lib/acme/31337.im/fullchain.pem:/ssl/fullchain.pem:ro"
+        "/var/lib/acme/31337.im/key.pem:/ssl/privkey.pem:ro"
+      ];
+      ports = [
+        "80:80/tcp"
+        "443:443/tcp"
+      ];
+      log-driver = "journald";
+      extraOptions = [
+        "--cap-add=NET_ADMIN"
+        "--network-alias=caddy"
+        "--network=komodo_komodo-internal"
+      ];
     };
-    labels = {"komodo.skip" = "";};
-    extraOptions = [
-      "--network-alias=periphery"
-      "--network=komodo_komodo-internal"
-    ];
-  };
-  
-  virtualisation.oci-containers.containers."komodo-core" = {
-    image = "ghcr.io/moghtech/komodo-core:latest";
-    environmentFiles = [
-      "/docker-data/Komodo/.env"
-    ];
-    # volumes = [
-    #   "/docker-data/Komodo/repo-cache:/repo-cache:rw"
-    # ];
-    labels = {
-      "komodo.skip" = "";
+
+    "komodo-periphery" = {
+      image = "ghcr.io/moghtech/komodo-periphery:latest";
+      labels = {"komodo.skip" = "";};
+      environmentFiles = [
+        "/docker-data/Komodo/.env"
+      ];
+      volumes = [
+          "/var/run/docker.sock:/var/run/docker.sock"
+          
+          "/etc/komodo/ssl:/etc/komodo/ssl"
+          "/etc/komodo/repos:/etc/komodo/repos"
+          "/etc/komodo/stacks:/etc/komodo/stacks"
+          "/var/secrets/komodo-passkey:/var/secrets/passkey"
+      ];
+      log-driver = "local";
+      extraOptions = [
+        "--network-alias=periphery"
+        "--network=komodo_komodo-internal"
+      ];
     };
-    dependsOn = [
-      "komodo-mongo"
-    ];
-    log-driver = "local";
-    extraOptions = [
-      "--network-alias=komodo-core"
-      "--network=komodo_komodo-internal"
-    ];
-  };
-  
-  virtualisation.oci-containers.containers."komodo-mongo" = {
-    image = "mongo";
-    environmentFiles = [
-      "/docker-data/Komodo/.env"
-    ];
-    volumes = [
-      "/docker-data/Komodo/mongodb/config:/data/configdb:rw"
-      "/docker-data/Komodo/mongodb/data:/data/db:rw"
-    ];
-    cmd = [ "--quiet" "--wiredTigerCacheSizeGB" "0.25" ];
-    labels = {
-      "komodo.skip" = "";
+    
+    "komodo-core" = {
+      image = "ghcr.io/moghtech/komodo-core:latest";
+      labels = {"komodo.skip" = "";};
+      environmentFiles = [
+        "/docker-data/Komodo/.env"
+      ];
+      dependsOn = [
+        "komodo-mongo"
+      ];
+      log-driver = "local";
+      extraOptions = [
+        "--network-alias=komodo-core"
+        "--network=komodo_komodo-internal"
+      ];
     };
-    log-driver = "local";
-    extraOptions = [
-      "--network-alias=mongo"
-      "--network=komodo_komodo-internal"
-    ];
+    
+    "komodo-mongo" = {
+      image = "mongo";
+      labels = {"komodo.skip" = "";};
+      environmentFiles = [
+        "/docker-data/Komodo/.env"
+      ];
+      volumes = [
+        "/docker-data/Komodo/mongodb/config:/data/configdb:rw"
+        "/docker-data/Komodo/mongodb/data:/data/db:rw"
+      ];
+      cmd = [ "--quiet" "--wiredTigerCacheSizeGB" "0.25" ];
+      log-driver = "local";
+      extraOptions = [
+        "--network-alias=mongo"
+        "--network=komodo_komodo-internal"
+      ];
+    };
   };
 
   # Networks
