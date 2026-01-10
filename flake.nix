61b9a86a6d88cf729e5581a464147aa45c148c19
Cody James <cody@cjtech.io>
2026-01-09 20:10:31 -0500

    test

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     ssh-keys = {
       url = "https://github.com/codyjamestechnical.keys";
       flake = false;
