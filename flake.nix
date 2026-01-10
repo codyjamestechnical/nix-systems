a5cddec7e36f7af1eb339f5d675e7fa188e16ef2
Cody James <cody@cjtech.io>
2026-01-09 19:18:05 -0500

    test

--- flake.nix
+++ flake.nix
@@ -2,29 +2,27 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    sops-nix.url = "github:Mic92/sops-nix";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     ssh-keys = {
       url = "https://github.com/codyjamestechnical.keys";
       flake = false;
     };
   };
 
-  outputs = inputs@{ nixpkgs, sops-nix, ssh-keys,... }: {
+  outputs = { self, nixpkgs, ssh-keys,... }@inputs: {
     nixosConfigurations = {
       mars-server = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/mars-server/default.nix
-          sops-nix.nixosModules.sops
         ];
       };
 
       deimos-server = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
+        specialArgs = inputs;
         modules = [
           ./hosts/deimos-server
-          sops-nix.nixosModules.sops
           
         ];
       };
