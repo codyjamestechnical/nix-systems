042e922bd6f27ef8d234995d02bf575743a18157
Cody James <cody@cjtech.io>
2026-01-16 17:16:50 -0500

    removed cifs config from headscale server

--- hosts/headscale-server/default.nix
+++ hosts/headscale-server/default.nix
@@ -17,15 +17,15 @@
     # Network HostName
     networking.hostName = "headscale-server";
 
-    fileSystems."/backup" = {
-        device = "//u429456.your-storagebox.de/backup";
-        fsType = "cifs";
-        options = let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,file_mode=0750,dir_mode=0750,uid=995,gid=131,mfsymlinks,seal,noperm,nobrl";
+    # fileSystems."/backup" = {
+    #     device = "//u429456.your-storagebox.de/backup";
+    #     fsType = "cifs";
+    #     options = let
+    #     # this line prevents hanging on network split
+    #     automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,file_mode=0750,dir_mode=0750,uid=995,gid=131,mfsymlinks,seal,noperm,nobrl";
 
-        in ["${automount_opts},credentials=/etc/nixos/secrets/smb-secrets"];
-    };
+    #     in ["${automount_opts},credentials=/etc/nixos/secrets/smb-secrets"];
+    # };
 
     system.stateVersion = "25.11";
 }
