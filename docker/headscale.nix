07485fcd8857d1d96f06889128fedb962cef7bb9
Cody James <cody@cjtech.io>
2026-02-01 11:40:51 -0500

    test

--- docker/headscale.nix
+++ docker/headscale.nix
@@ -146,7 +146,7 @@ in
         
       ];
       environment = {
-        HEADSCALE_ADDRESS = "headscale.cjtech.io:50443";
+        HEADSCALE_ADDRESS = "https://headscale.cjtech.io:50443";
         HEADSCALE_INSECURE = "true";
         LISTEN_ADDRESS = "0.0.0.0:9250";
       };
