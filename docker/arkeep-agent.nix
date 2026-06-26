66f43666f1f1b4e82e8de4e138dbd26dc3510981
Cody James <cody@cjtech.io>
2026-06-26 16:47:50 -0400

    added arkeep agent

---
+++ docker/arkeep-agent.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+let
+  cfg = {
+    service_name = "arkeep-agent";
+    network_name = "arkeep-internal";
+    secrets_dir = "/etc/nixos/secrets";
+    volumes = [
+      "/root/.arkeep:/var/lib/arkeep-agent"
+      "/docker-data:/backups/docker-data:rw"
+    ];
+  };
+in
+{
+  # Containers\
+  virtualisation.oci-containers.backend = "docker";
+  virtualisation.oci-containers.containers = {
+
+    ### ARKEEP AGENT ###
+    "${cfg.service_name}" = {
+      image = "ghcr.io/arkeep-io/arkeep-agent:latest";
+      extraOptions = [
+        "--network=host"
+      ];
+      volumes = cfg.volumes;
+      environmentFiles = [
+        "${cfg.secrets_dir}/arkeep-agent.env"
+      ];
+      labels = {
+        "komodo.skip" = "";
+      };
+    };
+
+  };
+}
