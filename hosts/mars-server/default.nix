dcf5d06b57caba05377683256e26f9c330ad9dbe
Cody James <cody@cjtech.io>
2026-09-08 14:27:07 -0400

    added udev rules for nvme for smart access

--- hosts/mars-server/default.nix
+++ hosts/mars-server/default.nix
@@ -92,7 +92,12 @@
           "/dev/nvme1:/dev/nvme1"
         ];
     };
-
+    
+    # Create a udev rule that changes NVMe devices to disk group ownership
+    # for Beszel to be able to show them correctly
+    udev.extraRules = ``
+      KERNEL=="nvme[0-9]*", GROUP="disk", MODE="0660"
+    ``
     ### ARKEEP PHOTO DIRECTORY ADDITION ###
     services.arkeep-agent.extraVolumes = [
         "/cjt_pool/Photo_Library:/hostfs/photo-library:ro"
