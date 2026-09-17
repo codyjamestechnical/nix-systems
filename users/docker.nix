416c007320f40a05847ea73968840e32030baa58
Cody James <cody@cjtech.io>
2026-09-17 11:14:39 -0400

    updated docker user with static group ID

--- users/docker.nix
+++ users/docker.nix
@@ -4,14 +4,17 @@
     defaultUserShell = pkgs.zsh;
     groups = {
       acme.gid = 984;
+      docker.gid = 131;
     };
 
     users.docker = {
       isNormalUser = false;
       isSystemUser = true;
       group = "docker";
+      uid = 991;
       extraGroups = [
         "acme"
+        "docker"
       ];
     };
   };
