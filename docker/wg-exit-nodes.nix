516ae8fcad8bc506dc40cac340b60799b40a6cf7
Cody James <cody@cjtech.io>
2026-09-25 15:20:36 -0400

    removed redundant code

--- docker/wg-exit-nodes.nix
+++ docker/wg-exit-nodes.nix
@@ -219,12 +219,6 @@ in
         wantedBy = [ "multi-user.target" ];
       }) enabledInstances)
 
-      # MERGE: Make gluetun wait for its network to exist before starting
-      # (mapAttrs' (name: inst: nameValuePair "${ociBin}-${inst.service_name}-gluetun" {
-      #   after = [ "${ociBin}-network-${inst.network_name}.service" ];
-      #   requires = [ "${ociBin}-network-${inst.network_name}.service" ];
-      # }) enabledInstances)
-
       // # MERGE: Extend the container services to delete TS_AUTHKEY after 1 minute
       (mapAttrs' (name: inst: nameValuePair "${ociBin}-${inst.service_name}" {
         # Background the delayed cleanup so ExecStartPost returns immediately.
