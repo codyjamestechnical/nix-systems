c1f35cd88fe1a7f36121144f951c0cdc34955956
Cody James <cody@cjtech.io>
2026-09-26 16:02:58 -0400

    test

--- users/cody.nix
+++ users/cody.nix
@@ -49,8 +49,6 @@
   ### ZSH SHELL ALIAS (cody only) ###
   programs.zsh.interactiveShellInit = ''
     if [ "$USER" = "cody" ]; then
-      # alias the podman command to use the podman user
-      alias podman="sudo -u podman podman"
       # Run podman as the podman user. Use a function instead of an
       # alias so we can cd to / first: rootless podman re-execs and
       # chdirs into the current working directory, which fails if we
